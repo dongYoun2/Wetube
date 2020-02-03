d4be6b0065390e03641f66466966e5846a7cd97f
dongYoun2 <ehddbs6425@naver.com>
2020-02-04 03:00:34 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+  {
+    id: 232453,
+    title: "Video awesome",
+    description: "This is sth I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "dong youn",
+      email: "dong@youn.com"
+    }
+  },
+  {
+    id: 232453,
+    title: "Video super",
+    description: "This is sth I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "dong youn",
+      email: "dong@youn.com"
+    }
+  },
+  {
+    id: 232453,
+    title: "Video nice",
+    description: "This is sth I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "dong youn",
+      email: "dong@youn.com"
+    }
+  },
+  {
+    id: 232453,
+    title: "Video perfect",
+    description: "This is sth I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "dong youn",
+      email: "dong@youn.com"
+    }
+  }
+];
