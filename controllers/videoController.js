f4e61d95a3f5a73988b1ef76a5a3a8753cccbcab
dongYoun2 <ehddbs6425@naver.com>
2020-02-05 18:08:40 +0900

    Comment Modeodel. Home Controller Finished.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,15 @@
 import routes from "../routes";
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", videos });
+import Video from "../models/Video";
+
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    res.render("home", { pageTitle: "Home", videos });
+  } catch (error) {
+    console.log(error);
+    res.render("home", { pageTitle: "Home", videos: [] });
+  }
+};
 
 export const search = (req, res) => {
   const {
@@ -13,11 +22,11 @@ export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 
 export const postUpload = (req, res) => {
-  const {
-    body: { file, title, description }
-  } = req;
+  const { body, file } = req;
+  console.log(file);
+  res.render("upload", { pageTitle: "Upload" });
   //To do: upload and save video
-  res.redirect(routes.videoDetail(121212));
+  //res.redirect(routes.videoDetail(121212));
 };
 
 export const videoDetail = (req, res) =>
