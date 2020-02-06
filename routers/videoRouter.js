0547334de219ece3bf3ec9b129eeebdf39b72bf5
dongYoun2 <ehddbs6425@naver.com>
2020-02-06 15:42:11 +0900

    Getting Video by ID

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,7 +3,8 @@ import routes from "../routes";
 import {
   getUpload,
   videoDetail,
-  editVideo,
+  getEditVideo,
+  postEditVideo,
   deleteVideo,
   postUpload
 } from "../controllers/videoController";
@@ -11,10 +12,18 @@ import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
+//Upload
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+
+//Video Detail
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+
+//Edit Video
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+
+//Delete Video
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 export default videoRouter;
