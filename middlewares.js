8b26d9b4d2ea223e6032dcd3d1316ecef9401cf9
dongYoun2 <ehddbs6425@naver.com>
2020-02-08 06:00:34 +0900

    Everything else

--- middlewares.js
+++ middlewares.js
@@ -7,6 +7,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.user = {
+    // isAuthenticated: false,
     isAuthenticated: false,
     id: 1
   };
