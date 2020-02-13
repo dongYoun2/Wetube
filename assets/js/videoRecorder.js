167b6d433b948520b6a1299ce77e3453eb4dea50
dongYoun2 <ehddbs6425@naver.com>
2020-02-13 10:26:08 +0900

    Recording video part Two

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -1,18 +1,36 @@
 const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
+const cameraBtn = document.getElementById("jsCameraBtn");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
-  console.log(event);
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", startRecording);
+  recordBtn.innerHTML = "Start Recording";
+  recordBtn.classList.remove("stop-record-btn");
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
   videoRecorder.addEventListener("dataavailable", handleVideoData);
-  setTimeout(() => videoRecorder.stop(), 3000);
+  recordBtn.removeEventListener("click", startRecording);
+  recordBtn.addEventListener("click", stopRecording);
+  recordBtn.innerHTML = "Stop Recording";
+  recordBtn.classList.add("stop-record-btn");
 };
 
 const getVideo = async () => {
@@ -24,18 +42,24 @@ const getVideo = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
-    recordBtn.innerHTML = "Stop recording";
     streamObject = stream;
-    startRecording();
+    // videoPreview.classList.add("show");
+    videoPreview.style.display = "inline";
+    cameraBtn.style.display = "none";
+    recordBtn.style.display = "inline-block";
+
+    recordBtn.addEventListener("click", startRecording);
+    // startRecording();
   } catch (error) {
     recordBtn.innerHTML = "😥Can't Record";
   } finally {
-    recordBtn.removeEventListener("click", getVideo);
+    cameraBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", getVideo);
+  cameraBtn.addEventListener("click", getVideo);
+  // recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
